2924084a065022c9c9942a912f6d92136463f08f
Charlie Lee <charlielee735@gmail.com>
2016-12-04 15:08:51 +0000

    Fix mobile menu not working when JavaScript switched off

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ var mobileToggle = document.getElementById("mobile-menu-toggle"),
     menuList     = document.getElementById("menu-list");
 
 mobileToggle.addEventListener("click", toggleMenu);
+window.addEventListener("load", toggleMenu);
 
 /**
  * Toggles the display of the navigation menu in mobile view
  */
 function toggleMenu() {
-  if (menuList.classList.contains("visible")) {
-    menuList.classList.remove("visible");
+  if (menuList.classList.contains("hidden")) {
+    menuList.classList.remove("hidden");
   } else {
-    menuList.classList.add("visible");
+    menuList.classList.add("hidden");
   }
 }
 

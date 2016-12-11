241408573291edbde311f61935791143c62c36c2
Charlie Lee <charlielee735@gmail.com>
2016-12-11 19:50:54 +0000

    pre tiling

--- js/pair-match.js
+++ js/pair-match.js
@@ -3,11 +3,13 @@ var gameArea = document.getElementById("game-area"),
     srcList  = ["1.jpg", "2.png", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"],
     imgList  = [];
 
+var totalImages = 0;
+
 var GRID_WIDTH  = 4,
     GRID_HEIGHT = 4;
 
-var totalImages = 0,
-    curRow = 1;
+var GRID_CELL_WIDTH = gameArea.width/GRID_WIDTH,
+    GRID_CELL_HEIGHT = gameArea.height/GRID_HEIGHT;
 
 
 
@@ -20,37 +22,58 @@ function createImage(name) {
   picture.src = "img/pair-match/" + name;
 
   // Load the image to the "imgList" array
-    picture.onload = function() {
-      totalImages++;
-      imgList.push(picture);
-      drawImage(picture, totalImages);
+  picture.onload = function() {
+    imgList.push(picture);
+    drawImage(picture, imgList.length);
+    totalImages == imgList.length;
+
+    if (imgList.length == srcList.length) {
+      console.log("YA");
     }
+  }
 }
 
 /**
- * Draw an image at a specified grid position
+ * Draw an image on the grid
  */
-function drawImage(picture, position) {
-  if (totalImages%4 == 0) curRow++
-  xPos = position&GRID_WIDTH;
-  yPos = curRow;
+function drawImage(picture, imgNo) {
+  // Position with the grid
+  var gridX = imgNo%GRID_WIDTH == 0 ? GRID_WIDTH : imgNo%GRID_WIDTH,
+      gridY = Math.ceil(imgNo/GRID_HEIGHT);
+
+
+  console.log(`AP:${imgNo} X:${gridX} and Y:${gridY}, ${picture.src.substr(-5)}`)
+
+  // Position on canvas
+  var canvasX = (gridX - 1) * GRID_CELL_WIDTH,
+      canvasY = (gridY - 1) * GRID_CELL_HEIGHT;
+
   context.drawImage(picture,
   // Crop the center of the of the image
   picture.width/8, picture.height/8, picture.width*3/4, picture.height*3/4,
   // Draw the image on the grid at the grid position specified
-  gameArea.width/GRID_WIDTH*xPos, gameArea.height/GRID_HEIGHT*yPos, gameArea.width/GRID_WIDTH, gameArea.height/GRID_HEIGHT);
+  canvasX, canvasY, GRID_CELL_WIDTH, GRID_CELL_HEIGHT);
 }
 
 
 function createGrid() {
   for (var i=0; i<srcList.length; i++) {
+    var tile = new Tile(srcList[i])
+
+
     createImage(srcList[i]);
   }
 }
 
 createGrid();
 
-function Square(position, imgSrc) {
-  this.position = position;
-  
+
+
+
+
+function Tile(img) {
+  this.img = img;
+  this.id = totalImages;
 }
+
+

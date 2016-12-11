48edc6361913423c0ff1a69e112af18d6e2fef42
Charlie Lee <charlielee735@gmail.com>
2016-12-11 20:40:01 +0000

    Update toggle code to use classList.toggle

--- js/main.js
+++ js/main.js
@@ -8,10 +8,6 @@ window.addEventListener("load", toggleMenu);
  * Toggles the display of the navigation menu in mobile view
  */
 function toggleMenu() {
-  if (menuList.classList.contains("hidden")) {
-    menuList.classList.remove("hidden");
-  } else {
-    menuList.classList.add("hidden");
-  }
+  menuList.classList.toggle("hidden");
 }
 
